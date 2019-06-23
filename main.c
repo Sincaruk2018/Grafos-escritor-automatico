731946b0f741cbc0265b14ae3920be51bf476048
Luiz Vieira <40367461+Sincaruk2018@users.noreply.github.com>
2019-06-22 22:38:25 -0300

    Update main.c

--- main.c
+++ main.c
@@ -19,47 +19,43 @@ int main() {
 
     printIntro();
 
-    for (int rept = 0; rept < 3; rept++) {
-        int qtdPalavra = 0;
-        char palavra[50];
-
-        char letra;
-        int posLetra = 0;
-        do{
-
-            letra =(char) getchar();
-            if (letra == ' ' || letra == '\n'){
-                if (posLetra != 0) {
-                    palavra[posLetra] = '\0';
-                    printf("[%s]\n", palavra);
-                    posLetra = 0;
-
-                    qtdPalavra++;
-                    frase = realloc(frase, qtdPalavra * sizeof(char*));
-                    frase[qtdPalavra-1] =(char*) malloc(50 * sizeof(char));
-                    strcpy(frase[qtdPalavra-1], palavra);
-                }
-            } else{
-                palavra[posLetra] = letra;
-                posLetra++;
-            }
-
-        }while (letra != '\n');
-
-        for(int i = 0; i < qtdPalavra; i++){
-            verticeInsere(g, frase[i]);
+  
+    FILE * dicionario;
+    dicionario = fopen("arquivo.txt","r"); // Nome padrao
+    if (dicionario ==  NULL)
+    {
+        return 1;
+    }
+    char primeiraPalavra[MAX] = "\0";
+    char auxProx[MAX] = "\0";
+    char c;
+    int origem, destino;
+    while (c != EOF)
+    {
+        if (strcmp(auxProx, "\0") == 0) // Se não houver próxima, é porque é a primeira da frase
+        {
+            fscanf(dicionario,"%s",primeiraPalavra);
         }
-
-        for(int i = 0; i < qtdPalavra; i++){
-            int j = i+1;
-            if(i == 0) arestaInsere(g, verticeBusca(g,":ini"), verticeBusca(g,frase[i]));
-            if( j == qtdPalavra) arestaInsere(g, verticeBusca(g, frase[i]), verticeBusca(g, ":fim"));
-            else arestaInsere(g, verticeBusca(g,frase[i]), verticeBusca(g, frase[j]));
-
-            free(frase[i]);
+        else
+        {
+            strcpy(primeiraPalavra,auxProx);
+        }
+        c = fgetc(dicionario);
+        if (c != 0xD) // Número binário para compatibilidade com Linux
+        {
+            fscanf(dicionario,"%s",auxProx);
+            /* Implementar a busca antes para evitar as mensagens de erro*/
+            verticeInsere(g, primeiraPalavra);
+            verticeInsere(g, auxProx);
+            origem = verticeBusca(g, primeiraPalavra);
+            destino = verticeBusca(g, auxProx);
+            arestaInsere(g, origem, destino);
+        }
+        else
+        {
+            strcpy(auxProx,"\0"); //Reset implicito
         }
     }
-
     grafoPrint(g);
 
     Djokstra(g, verticeBusca(g,":ini"), verticeBusca(g,":fim"));
